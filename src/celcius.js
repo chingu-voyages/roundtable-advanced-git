8d3cd5c54042bcd4d452811e35fe17826126e3e3
Andres Court <andres@andrescourt.com>
2025-01-07 21:54:02 -0500

    refactor: move the convert to celcius function to its own file

---
+++ src/celcius.js
@@ -0,0 +1,20 @@
+/**
+ * Convert a temperature that is in Farenheit to Celcius
+ *
+ * Formula used
+ * celcius = (farenheit - 32) * 5/9
+ *
+ * @param {number} temp The temperature in farenheit
+ * @returns {number} The converted celcius value
+ *
+ * @author Andres Court
+ */
+function ConverToCelcius(temp) {
+    /** @type {number} */
+    var value = temp - 32
+    value = value * 5 / 9
+
+    return value
+}
+
+module.exports = ConverToCelcius
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-
-/**
- * Convert a temperature that is in Farenheit to Celcius
- *
- * Formula used
- * celcius = (farenheit - 32) * 5/9
- *
- * @param {number} temp The temperature in farenheit
- * @returns {number} The converted celcius value
- *
- * @author Andres Court
- */
-function ConverToCelcius(temp) {
-    /** @type {number} */
-    var value = temp - 32
-    value = value * 5 / 9
-
-    return value
-}
 const ConvertToFarenheit = require("./farenheit.js")
+const ConverToCelcius = require("./celcius.js")
 
 const far = ConvertToFarenheit(21)
-const cel = ConverToCelcius(far)
+const cel = ConverToCelcius(69.8)
 
 console.log(far)
 console.log(cel)
