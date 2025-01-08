84cb27ba5b93c35205025e44331ae03f8bbe40d2
Andres Court <andres@andrescourt.com>
2025-01-07 21:32:44 -0500

    feat: improved jsdoc documentation of the functions

--- src/index.js
+++ src/index.js
@@ -9,10 +9,13 @@
  * Formula used
  * farenheit = 32 + (9/5 * celcius)
  *
- * @param {number} temp 
- * @returns {number}
+ * @param {number} temp The temperature in celcius
+ * @returns {number} The converted farenheit value
+ *
+ * @author Andres Court
  */
 function ConvertToFarenheit(temp) {
+    /** @type {number} */
     let value = temp * 9 / 5;
     value = value + 32
 
@@ -25,10 +28,13 @@ function ConvertToFarenheit(temp) {
  * Formula used
  * celcius = (farenheit - 32) * 5/9
  *
- * @param {number} temp 
- * @returns {number}
+ * @param {number} temp The temperature in farenheit
+ * @returns {number} The converted celcius value
+ *
+ * @author Andres Court
  */
 function ConverToCelcius(temp) {
+    /** @type {number} */
     var value = temp - 32
     value = value * 5 / 9
 
