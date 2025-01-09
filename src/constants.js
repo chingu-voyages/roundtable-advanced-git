c4e6030cd8489575256b43351663aec2588db8f4
Andres Court <andres@andrescourt.com>
2025-01-08 21:35:08 -0500

    feat: use constants for celcius conversions

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+/** @type {number} conversion factor from farenheit to celcius */
+export const CelciusFactor = 5;
+
+/** @type {number} Offset between celcius and farenheit zero value */
+export const TemperatureOffset = 32;
