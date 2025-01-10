71e7358101347503a1d2c704933fd7c8821161ec
Andres Court <andres@andrescourt.com>
2025-01-09 19:00:50 -0500

    refactor: move the celcius and farenheit functions to its own directory

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 /** @type {number} conversion factor from farenheit to celcius */
 export const CelciusFactor = 5;
 
+/** @type {number} conversion factor from celcius to farenheit */
+export const FarenheitFactor = 9 / 5;
+
 /** @type {number} Offset between celcius and farenheit zero value */
 export const TemperatureOffset = 32;
