7dc9a8977ae6bfeaf817e8cf8b799feb76af26cb
Andres Court <andres@andrescourt.com>
2025-01-11 09:47:08 -0500

    feat: move the convert to meters to its own file
    Signed-of-by: Andres Court \ <andres@test.com>
    Helped-by: Jim Medlock \ <jim@test.com>

---
+++ src/distance/ConvertToMeters.js
@@ -0,0 +1,12 @@
+import { DistanceFactor } from "../constants";
+
+/**
+ * Converts a distance expressed in feet to meters
+ *
+ * @type val {number} distance in feet
+ * @returns {number} the distance in meters
+ */
+
+export function ConvertToMeters(val) {
+    return val / DistanceFactor;
+}
