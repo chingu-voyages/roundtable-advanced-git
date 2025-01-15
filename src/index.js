155a26df07365bd6215eaa5028a983a95e766245
Andres Court <andres@andrescourt.com>
2025-01-15 16:09:28 -0500

    add assertions about distance conversion

--- src/index.js
+++ src/index.js
@@ -11,3 +11,5 @@ const feet = ConvertToFeet(0.9144)
 
 console.log(far);
 console.log(cel);
+console.assert(Math.abs(0.9144 - met) < 0.0005, `The value should be close to 0.9144 meters and it was ${met}`)
+console.assert(Math.abs(3 - feet) < 0.0005, `The value should be close to 3 feet and it was ${feet}`)
