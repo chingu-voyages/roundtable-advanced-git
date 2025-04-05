4e2d6de2ed548eaf84f403652ebd31069637769d
Andres Court <andres@andrescourt.com>
2025-04-05 09:48:38 -0500

    fix(constants): update the CelciusFactor

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /** @type {number} conversion factor from farenheit to celcius */
-export const CelciusFactor = 5;
+export const CelciusFactor = 5 / 9;
 
 /** @type {number} Offset between celcius and farenheit zero value */
 export const TemperatureOffset = 32;
